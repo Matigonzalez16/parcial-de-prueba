8c790d13b44ac1db3b6400d1d5b9ebdfbb95d15c
alumno <alumno@utnfra.com.ar>
2020-02-26 22:18:04 -0300

    ejercicio 3

--- Modelo_Parcial_Ingreso/dos.js
+++ Modelo_Parcial_Ingreso/dos.js
@@ -5,29 +5,73 @@ function mostrar()
   var edad;
   var genero;
   var peso; 
+  var acumuladorEdades = 0;
+  var cantidadMayorA25 = 0;
+  var promedio = 0;
+  var mensaje = " ";
+  var personaMenosPesada;
+  var pesoMinimo;
+  var esPrimeraIteracion = true;
+  var cantidadPersonas = 0;
+
+
 
 for (persona = 0; persona <5; persona ++)
 { 
   	nombre = prompt("Ingrese nombre: ");
   	
-  	genero = prompt("Ingrese el genero : (masculino/femenino/otros");
+  	genero = prompt("Ingrese el genero : (masculino/femenino/otros").toLowerCase;
   	while (genero != "femenino" && genero != "masculino" && genero != "otro") {
-  		genero = prompt(" ERROR. Ingrese el genero : (masculino/femenino/otros");
+  		genero = prompt(" ERROR. Ingrese el genero : (masculino/femenino/otros").toLowerCase;
   	}
 
   	edad = prompt("Ingrese edad: ");
   	edad = parseInt(edad);
   	while (edad < 18 || edad > 100 ) {
-  	alert("Ingrese una edad valida");
-  	edad = prompt("Ingrese una edad: ");
+  	edad = prompt("ERROR,Ingrese una edad valida: ");
   	}
   	
 	
-	peso = prompt("Ingrese peso");
+	peso = prompt("Ingrese peso (Mayor a 1");
 	peso = parseInt(peso);
-	while (peso < 1) {
-		alert("Ingrese un peso valido");
-	peso = prompt("ingrese pepesoo");
+	while (isNaN(peso) ||  peso <= 1) {
+	peso = prompt("ERROR, ingrese peso mayor a 1");
 	}
-										}
+							
+	if (edad >= 25) {
+		acumuladorEdades += edad;
+		cantidadMayorA25++;
+	}
+
+
+// proceso los datos
+
+if (cantidadMayorA25 >0) {
+	promedio = acumuladorEdades / cantidadMayorA25;
+	mensaje += "El promedio de edades mayores a 25 es" + promedio;
 }
+else 
+{
+	mensaje += "No se ingresaron edades mayores a 25";
+
+}
+if (esPrimeraIteracion || peso < pesoMinimo){
+	pesoMinimo = peso;
+	personaMenosPesada = nombre;
+}
+//entiendo que el 80 no esta incluido
+if (genero != "masculino" ||peso > 80)
+{
+	cantidad de personas 
+}
+
+esPrimeraIteracion = false;
+}
+
+
+//  flags/banderas
+// var tieneCorreo;   tiene o no tiene (true o false)
+// 
+
+
+
--- Modelo_Parcial_Ingreso/tres.js
+++ Modelo_Parcial_Ingreso/tres.js
@@ -1,4 +1,33 @@
 function mostrar()
 {
+var pais;
+var habitantes;
+var temperaturaMin;
+var promedio;
+var acumuladorTempMin = 0;
+var tempImpar = 0;
+
+
+
+pais = prompt("Ingrese nombre del pais");
+
+habitantes = prompt("Ingrese numero de habitantes");
+habitantes = parseInt(habitantes);
+while ( habitantes < 1000000  || habitantes > 270000000) {
+	habitantes = prompt("ERROR. Ingrese numero de habitantes correcto entre 1 y 270 millones");
+}
+
+temperaturaMin = prompt("Ingrese temperatura minima");
+temperaturaMin = parseInt(temperaturaMin);
+while (temperaturaMin > -50 || temperaturaMin > 50) {
+	temperaturaMin = prompt("ERROR, Ingrese una temperatura valida (entre -50º y 50º");
+}
+
+if (temperaturaMin % 2 != 0){
+	acumuladorTempMin += temperaturaMin;
+	tempImpar ++;
+}
+
+
 
 }
